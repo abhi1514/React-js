addbe18b13382430f25cc88442e480e92eddd7ee
Abhishek <abhishekbachate07@.in>
2025-08-12 12:36:35 +0530

    input type

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import User from './User'
-import Name from './New'
-import Login from './New'
+// import Name from './New'
+// import Login from './New'
 // import Btn from './User'
 
 
@@ -23,21 +23,21 @@ function App() {
        <h1 id='ab'>{fruit}</h1>
        <button onClick={change}>Change</button>
        </div>
-    <User/> 
-    <Name/>
-    <Input/>
-    <Counter/>
+    <User /> 
+    {/* <Name/> */}
+    {/* <Input/> */}
+    {/* <Counter/> */}
     </>
   )
 }
 
-function Input(){
-  return(
-    <div>
-      <h3> Welcome React</h3>
+// function Input(){
+//   return(
+//     <div>
+//       <h3> Welcome React</h3>
    
-    </div>)
-}
+//     </div>)
+// }
 
 
 export default App;
--- src/User.jsx
+++ src/User.jsx
@@ -1,23 +1,28 @@
-function User(){
-    function Num(age){
-      if(age>=18){
-        return "you can drive"
-      }
-      else if(age<18 && age>=16){
-        return "you cannot apply driving license "
-      }
-       else{
-        return 'you are kid'
-       }
-    }
-    return(
+import { useState } from "react";
+// function User({name,age,Email}){
+    function User(){
+    const[Name,setName]=useState('')
+    const[Pass,setPass]=useState('')
+    const[Email,setEmail]=useState('')
+       return(
+        <>
         <div>
-            <h2> React js+vite</h2>
-            <h1>{Num(16)}</h1>
+            <label >
+            <input type="checkbox"  />Java
+            </label><br></br>
+          <input type="text" value={Name} onChange={(event)=>setName(event.target.value)} placeholder="Enter  Name"/><br></br>
+          <input type="text" value={Pass} onChange={(event)=> setPass(event.target.value)} placeholder="Enter password" /><br></br>
+          <input type="text" value={Email} onChange={(event)=>setEmail(event.target.value)} placeholder="Enter Email" /><br></br><hr></hr>
+          <button onClick={()=>{setName("");setPass("");setEmail("")}}>Clear</button>
+            <h3> age:{Pass}</h3>
+            <h3>Name:{Name}</h3>
+            <h3>Email:{Email}</h3>
         </div>
+        
+</>
     )
     
-}
 
+}
 
 export default User ;
